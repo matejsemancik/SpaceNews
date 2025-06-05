ae6c2675b28f133ea9aa63bfabdb7336b570d8e5
matejsemancik <matej.semancik@gmail.com>
2025-06-05 21:16:47 +0200

    Fix iOS build

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import ComposeApp
+import SpaceNewsKit
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
--- shared/src/iosMain/kotlin/dev/matsem/spacenews/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/dev/matsem/spacenews/shared/MainViewController.kt
@@ -1,3 +1,5 @@
 package dev.matsem.spacenews.shared
 
+import androidx.compose.ui.window.ComposeUIViewController
+
 fun MainViewController() = ComposeUIViewController { App() }
